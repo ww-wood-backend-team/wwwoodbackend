c0143e3e3a6e5bcf9a4df0014deb49e71a2632a5
dman620 <homeses@gmail.com>
2019-03-25 08:24:47 -0700

    first commit

---
+++ wwwood-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wwwood_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
