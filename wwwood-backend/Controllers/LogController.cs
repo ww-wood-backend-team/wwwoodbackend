e06842ad5d98f0187fcaec2d9970e52e2e9f0a27
simoneruffin <simone0934567@yahoo.com>
2019-04-09 03:57:28 -0500

    FilterLogs contoller should be mostly functional. If you do not want to filter something, just put null inplace of the keyword, type or dates.

--- wwwood-backend/Controllers/LogController.cs
+++ wwwood-backend/Controllers/LogController.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using wwwoodbackend.Models;
 using Microsoft.AspNetCore.Cors;
 using System.Data.SqlClient;
 using System.Data;
-using Microsoft.Extensions.Configuration;
 using System;
 
 namespace wwwoodbackend.Controllers
@@ -17,65 +13,8 @@ namespace wwwoodbackend.Controllers
     [ApiController]
     public class LogController : ControllerBase
     {
-        //private readonly LogContext _context;
-        //private readonly IConfiguration configuration;
 
-        //private SqlConnection _conn;
         private SqlDataAdapter _adapter;
-        //public FirstController(IConfiguration config)
-        //{
-        //    this.configuration = config;
-        //}
-
-        //public IEnumerable<Log> Get()
-        //{
-        //    string connectionString = configuration.GetConnectionString("WWWoodDatabase");
-        //    SqlConnection connection = new SqlConnection(connectionString);
-        //    connection.Open();
-        //    SqlCommand command = new SqlCommand("select (*) from ApplicationLogs");
-        //    connection.Close();
-        //    return (List < Log >)command.ExecuteScalar();
-
-        //}
-        //public IEnumerable<Log> Get()
-        //{
-        //    _conn = new SqlConnection("Server=13.65.80.168;Database=Auditing;User Id=semocapstone;Password=Capstone2019;Connection Timeout=60");
-        //    DataTable _dt = new DataTable();
-        //    var query = "select * from ApplicationLogs";
-        //    _adapter = new SqlDataAdapter
-        //    {
-        //        SelectCommand = new SqlCommand(query, _conn)
-        //    };
-
-        //    _adapter.Fill(_dt);
-        //    List<Log> logs = new List<Models.Log>(_dt.Rows.Count);
-
-        //    if(_dt.Rows.Count>0)
-        //    {
-        //        foreach(DataRow logrecord in _dt.Rows)
-        //        {
-        //            logs.Add(new ReadLog(logrecord));
-        //        }
-
-        //    }
-        //    return logs;
-
-        //}
-
-        //public LogController(LogContext context)
-        //{
-        //    _context = context;
-
-        //    //if (_context.Logs.Count() == 0)
-        //    //{
-        //        ////// Create a new LogItem if collection is empty,
-        //        //// which means you can't delete all LogItems.
-        //        ////_context.Logs.Add(new Log { LogEntryId=1, SessionId = "1", AssemblyId="1", NotifyIncident="1"  });
-        //        //_context.Logs.Add(new Log { LogEntryId = 2, SessionId = "2", AssemblyId = "2", NotifyIncident = "2" });
-        //        ////_context.Logs.Add(new Log { Message = "Item3" });
-        //        //_context.SaveChanges();
-        //   // }
-        //}
 
         /////Refresh every ten seconds
 
@@ -95,25 +34,100 @@ namespace wwwoodbackend.Controllers
         ///will search for a log with keyword "keyword" -- not functional yet, I'm not sure this is what it was supposed to be.  I cannot remember.
         ///Not built to withstand incorrect inputs yet, so just make make sure inputs are correct when testing.
         [HttpGet("from={from}&to={to}&type={type}&search={keyword}")]
+
         [EnableCors("AllowAllHeaders")]
         public IEnumerable<SlimLog> FilterLogs(string from,string to,string type,string keyword) 
         {
 
             SqlConnection connection = new SqlConnection("Server=13.65.80.168,1433;Database=Auditing;User Id=semocapstone;Password=Capstone2019;Connection Timeout=300");
-            connection.Open();
-                DataTable _dt = new DataTable();
-            var queryString =
-                "SELECT TOP (10) * FROM [Auditing].[dbo].[ApplicationLogs] WHERE [LogEntryType] = '" + type + "' AND [Timestamp] >= '" + from + " 00:00:00 ' AND [Timestamp] <= '" + to + " 23:59:59 '";
+            connection.Open(); 
+            DataTable _dt = new DataTable();
+
             //Selects top 10 for now, Simone says there is a timeout issue right now.
-            //"SELECT TOP (10) * FROM [Auditing].[dbo].[ApplicationLogs]" +
-            ////Searches for the log with the matching type
-            //"WHERE [LogEntryType] = " + type + " and " +
-            ////Searches for a log from midnight on the earliest date to ll:59:59PM on the last date
-            //"[Timestamp] >= " + from + " 00:00:00 and [Timestamp] <= " + to + " 23:59:59 ";
-            _adapter = new SqlDataAdapter
+
+
+            //Example: /api/logs/from=2019-01-01&to=2019-01-20&type=Information&search=false
+            var queryStringFilterall =
+                "SELECT TOP (10) * FROM [Auditing].[dbo].[ApplicationLogs] WHERE [LogEntryType] = '" + type + "' AND [Timestamp] >= '" + from + " 00:00:00 ' AND [Timestamp] <= '" + to + " 23:59:59 ' AND [Message] LIKE '%" + keyword+"%'";
+
+            //Example: /api/logs/from=2019-01-01&to=2019-01-20&type=null&search=null
+            var queryStringFilterDate =
+                "SELECT TOP (10) * FROM [Auditing].[dbo].[ApplicationLogs] WHERE [Timestamp] >= '" + from + " 00:00:00 ' AND [Timestamp] <= '" + to + " 23:59:59 '";
+
+            //Example: /api/logs/from=null&to=null&type=null&search=false
+            var queryStringFilterSearch =
+                "SELECT TOP (10) * FROM [Auditing].[dbo].[ApplicationLogs] WHERE " +
+                "[Message]" +
+                "LIKE '%" + keyword + "%'";
+
+            //Example: /api/logs/from=null&to=null&type=information&search=null
+            var queryStringFilterType =
+                "SELECT TOP (10) * FROM [Auditing].[dbo].[ApplicationLogs] WHERE [LogEntryType] = '" + type + "'";
+
+            //Example: /api/logs/from=2019-01-01&to=2019-01-20&type=information&search=null
+            var queryStringFilterNoSearch =
+                "SELECT TOP (10) * FROM [Auditing].[dbo].[ApplicationLogs] WHERE [LogEntryType] = '" + type + "' AND [Timestamp] >= '" + from + " 00:00:00 ' AND [Timestamp] <= '" + to + " 23:59:59 '";
+
+            //Example: /api/logs/from=2019-01-01&to=2019-01-20&type=null&search=false
+            var queryStringFilterNoType =
+                "SELECT TOP (10) * FROM [Auditing].[dbo].[ApplicationLogs] WHERE [Timestamp] >= '" + from + " 00:00:00 ' AND [Timestamp] <= '" + to + " 23:59:59 ' AND [Message] LIKE '%" + keyword + "%'";
+
+            //Example: /api/logs/from=null&to=null&type=Information&search=false
+            var queryStringFilterNoDate =
+                "SELECT TOP (10) * FROM [Auditing].[dbo].[ApplicationLogs] WHERE [LogEntryType] = '" + type + "' AND [Message] LIKE '%" + keyword + "%'";
+
+
+
+            if (type.Equals("null", StringComparison.InvariantCultureIgnoreCase) && keyword.Equals("null", StringComparison.InvariantCultureIgnoreCase))
             {
-                SelectCommand = new SqlCommand(queryString, connection)
-            };
+                _adapter = new SqlDataAdapter
+                {
+                    SelectCommand = new SqlCommand(queryStringFilterDate, connection)
+                };
+            }
+
+            else if(type.Equals("null", StringComparison.InvariantCultureIgnoreCase) && to.Equals("null", StringComparison.InvariantCultureIgnoreCase) && from.Equals("null", StringComparison.InvariantCultureIgnoreCase))
+            {
+                _adapter = new SqlDataAdapter
+                {
+                    SelectCommand = new SqlCommand(queryStringFilterSearch, connection)
+                };
+            }
+            else if(to.Equals("null", StringComparison.InvariantCultureIgnoreCase) && from.Equals("null", StringComparison.InvariantCultureIgnoreCase) && keyword.Equals("null", StringComparison.InvariantCultureIgnoreCase))
+            {
+                _adapter = new SqlDataAdapter
+                {
+                    SelectCommand = new SqlCommand(queryStringFilterType, connection)
+                };
+            }
+            else if (!type.Equals("null", StringComparison.InvariantCultureIgnoreCase) && !to.Equals("null", StringComparison.InvariantCultureIgnoreCase) && !from.Equals("null", StringComparison.InvariantCultureIgnoreCase) && keyword.Equals("null", StringComparison.InvariantCultureIgnoreCase))
+            {
+                _adapter = new SqlDataAdapter
+                {
+                    SelectCommand = new SqlCommand(queryStringFilterNoSearch, connection)
+                };
+            }
+            else if (!keyword.Equals("null", StringComparison.InvariantCultureIgnoreCase) && !to.Equals("null", StringComparison.InvariantCultureIgnoreCase) && !from.Equals("null", StringComparison.InvariantCultureIgnoreCase) && type.Equals("null", StringComparison.InvariantCultureIgnoreCase))
+            {
+                _adapter = new SqlDataAdapter
+                {
+                    SelectCommand = new SqlCommand(queryStringFilterNoType, connection)
+                };
+            }
+            else if (!keyword.Equals("null", StringComparison.InvariantCultureIgnoreCase) && !type.Equals("null", StringComparison.InvariantCultureIgnoreCase) && to.Equals("null", StringComparison.InvariantCultureIgnoreCase) && from.Equals("null", StringComparison.InvariantCultureIgnoreCase))
+            {
+                _adapter = new SqlDataAdapter
+                {
+                    SelectCommand = new SqlCommand(queryStringFilterNoDate, connection)
+                };
+            }
+            else
+            {
+                _adapter = new SqlDataAdapter
+                {
+                    SelectCommand = new SqlCommand(queryStringFilterall, connection)
+                };
+            }
 
             _adapter.Fill(_dt);
 
@@ -135,8 +149,7 @@ namespace wwwoodbackend.Controllers
         [EnableCors("AllowAllHeaders")]
         public IEnumerable<SlimLog> ListLogs()
         {
-            //string connectionString = configuration.GetConnectionString("WWWoodDatabase");
-            //_conn = new SqlConnection("Server=13.65.80.168;Database=Auditing;User Id=semocapstone;Password=Capstone2019;Connection Timeout=60");
+
 
             SqlConnection connection = new SqlConnection("Server=13.65.80.168,1433;Database=Auditing;User Id=semocapstone;Password=Capstone2019;Connection Timeout=300");
 
@@ -222,51 +235,7 @@ namespace wwwoodbackend.Controllers
             return logs;
         }
 
-    ////// POST: api/Logs
-    //[HttpPost]
-    //[EnableCors("AllowAllHeaders")]
-    //public async Task<ActionResult<Log>> PostLog(Log log)
-    //{
-    //    _context.ApplicationLogs.Add(log);
-    //    await _context.SaveChangesAsync();
 
-    //    return CreatedAtAction(nameof(SelectLog), new { id = log.LogEntryId }, log);
-    //}
-
-    ////// PUT: api/Logs/5
-    //[HttpPut("{id}")]
-    //[EnableCors("AllowAllHeaders")]
-    //public async Task<IActionResult> PutLogs(long id, Log log)
-    //{
-    //    if (id != log.LogEntryId)
-    //    {
-    //        return BadRequest();
-    //    }
-
-    //    _context.Entry(log).State = EntityState.Modified;
-    //    await _context.SaveChangesAsync();
-
-    //    return NoContent();
-    //}
-
-    ////// DELETE: api/Logs/5
-    //[HttpDelete("{id}")]
-    //[EnableCors("AllowAllHeaders")]
-    //public async Task<IActionResult> DeleteLog(long id)
-    //{
-    //    var log = await _context.ApplicationLogs.FindAsync(id);
-
-    //    if (log == null)
-    //    {
-    //        return NotFound();
-    //    }
-
-    //    _context.ApplicationLogs.Remove(log);
-    //    await _context.SaveChangesAsync();
-
-    //    return NoContent();
-    //}
-
-}
+    }
 
 }
